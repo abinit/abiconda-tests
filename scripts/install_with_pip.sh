d024864c746575d1ac7748d8b15ceaabdcf9a193
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-25 15:47:05 +0100

    Upgrade pip

--- scripts/install_with_pip.sh
+++ scripts/install_with_pip.sh
@@ -3,9 +3,14 @@ set -ev  # exit on first error, print each command
 
 which pip
 pip --version
+pip install --upgrade pip
+pip --version
+pip check
 
+pip install six
 pip install abipy 
 #pip install ipython
+pip check
 
 abidoc.py man ecut
 abirun.py --help
