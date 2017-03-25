24faf438b48c8b0038590e3b18fbbc8ce5537ca3
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-25 19:25:23 +0100

    Activate matrix

--- scripts/install_with_conda.sh
+++ scripts/install_with_conda.sh
@@ -4,7 +4,7 @@ set -ev  # exit on first error, print each command
 # Replace dep1 dep2 ... with your dependencies
 # conda create -q -n test-environment python=${PYTHON_VERSION} dep1 dep2 ...
 test_env=test-py${PYTHON_VERSION}
-`conda env remove -n ${test_env} -y` || true
+conda env remove -n ${test_env} -y || true
 conda create -q -n ${test_env} python=${PYTHON_VERSION}
 source activate ${test_env}
 
@@ -22,4 +22,5 @@ conda install abipy
 abidoc.py man ecut
 abirun.py --help
 abicheck.py --help
+source deactivate ${test_env}
 conda env remove -n ${test_env} -y
