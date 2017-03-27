2b01cabb22fd72728584f58279c42d34f71c5de4
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-27 14:11:21 +0200

    Test other configurations

--- scripts/install_with_conda.sh
+++ scripts/install_with_conda.sh
@@ -13,15 +13,59 @@ source activate ${test_env}
 conda config --add channels conda-forge
 conda config --add channels matsci
 conda config --add channels abinit
-#conda config --add channels gmatteo
 
-# Install
+# Install from conda channels.
 conda install abinit
 conda install abipy
 
-# Test
+# Test Abipy scripts
 abidoc.py man ecut
 abirun.py --help
 abicheck.py --help
+#abicheck.py --with-flow
+
+# Test abipy/develop with pymatgen installed from conda.
+conda uninstall abipy
+
+git clone https://github.com/abinit/abipy.git 
+cd abipy && git checkout develop
+conda install -y --file ./requirements-optional.txt
+conda install -y --file ./requirements.txt
+python setup.py install && cd ../
+
+# Test Abipy scripts
+abidoc.py man ecut
+abirun.py --help
+abicheck.py --help
+#abicheck.py --with-flow
+
+
+# Test pymatgen/master with abipy/master
+conda uninstall pymatgen
+conda uninstall abipy
+
+git clone https://github.com/gmatteo/pymatgen.git
+cd pymatgen && git checkout master
+conda install -y --file ./requirements-optional.txt
+conda install -y --file ./requirements.txt
+python setup.py install && cd ../
+
+
+cd abipy && git checkout master
+conda install -y --file ./requirements-optional.txt
+conda install -y --file ./requirements.txt
+python setup.py install && cd ../
+
+# Test Abipy scripts
+abidoc.py man ecut
+abirun.py --help
+abicheck.py --help
+#abicheck.py --with-flow
+
+# Deactivate environment.
 source deactivate ${test_env}
 conda env remove -n ${test_env} -y
+
+#- git clone https://github.com/gmatteo/pymatgen.git && cd pymatgen && pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt && python setup.py install && cd ../
+# pymatgen master
+#- git clone https://github.com/materialsproject/pymatgen.git && cd pymatgen && pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt && python setup.py install && cd ../
