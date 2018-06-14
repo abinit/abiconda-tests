a9985be6b665153b9b6a49643613dd8645b6644c
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2018-06-15 00:11:29 +0200

    Pin pymatgen with pip

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Build Status](https://travis-ci.org/abinit/abiconda-tests.svg?branch=master)](https://travis-ci.org/abinit/abiconda-tests)
 
-A repository to periodically test software deployment and recipes.
+A repository to periodically test software deployment and conda recipes.
--- scripts/conda_runtests.sh
+++ scripts/conda_runtests.sh
@@ -27,59 +27,58 @@ abicheck.py --with-flow
 ######################################################
 # Test abipy/develop with pymatgen installed via conda
 ######################################################
-conda uninstall abipy
-git clone https://github.com/abinit/abipy.git 
-cd abipy && git checkout develop
-conda install -y --file ./requirements-optional.txt
-conda install -y --file ./requirements.txt
+#conda uninstall abipy
+#git clone https://github.com/abinit/abipy.git 
+#cd abipy && git checkout develop
+#conda install -y --file ./requirements.txt
+#conda install -y --file ./requirements-optional.txt
 
 # Create configuration files.
-mkdir -p ${HOME}/.abinit/abipy 
-cp abipy/data/managers/travis_scheduler.yml ${HOME}/.abinit/abipy/scheduler.yml
-cp abipy/data/managers/travis_manager.yml ${HOME}/.abinit/abipy/manager.yml
+#mkdir -p ${HOME}/.abinit/abipy 
+#cp abipy/data/managers/travis_scheduler.yml ${HOME}/.abinit/abipy/scheduler.yml
+#cp abipy/data/managers/travis_manager.yml ${HOME}/.abinit/abipy/manager.yml
 
-python setup.py install --record installed_files.txt && cd ../
+#python setup.py install --record installed_files.txt && cd ../
 
 # Test Abipy scripts
-abidoc.py man ecut
-abirun.py --help
-abicheck.py --with-flow
+#abidoc.py man ecut
+#abirun.py --help
+#abicheck.py --with-flow
 
 #################################################
 # Test abipy/master (stable) with pymatgen/master 
 #################################################
-conda uninstall pymatgen
-cat abipy/installed_files.txt | xargs rm -rf
-#conda uninstall abipy
+#conda uninstall pymatgen
+#cat abipy/installed_files.txt | xargs rm -rf
 
-git clone https://github.com/materialsproject/pymatgen.git
-cd pymatgen && git checkout master
-pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
-python setup.py install --record installed_files.txt && cd ../
+#git clone https://github.com/materialsproject/pymatgen.git
+#cd pymatgen && git checkout master
+#pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
+#python setup.py install --record installed_files.txt && cd ../
 
-cd abipy && git checkout master
-pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
-python setup.py install --record installed_files.txt && cd ../
+#cd abipy && git checkout master
+#pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
+#python setup.py install --record installed_files.txt && cd ../
 
 # Test Abipy scripts
-abidoc.py man ecut
-abirun.py --help
-abicheck.py
-abicheck.py --with-flow  
+#abidoc.py man ecut
+#abirun.py --help
+#abicheck.py
+#abicheck.py --with-flow  
 
 #########################################
 # Test abipy/develop with pymatgen/master 
 #########################################
-cat abipy/installed_files.txt | xargs rm -rf
-cd abipy && git checkout develop
-pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
-python setup.py install --record installed_files.txt && cd ../
+#cat abipy/installed_files.txt | xargs rm -rf
+#cd abipy && git checkout develop
+#pip install -q -r requirements.txt && pip install -q -r requirements-optional.txt 
+#python setup.py install --record installed_files.txt && cd ../
 
 # Test Abipy scripts
-abidoc.py man ecut
-abirun.py --help
-abicheck.py
-abicheck.py --with-flow  
+#abidoc.py man ecut
+#abirun.py --help
+#abicheck.py
+#abicheck.py --with-flow  
 
 # Deactivate environment.
 source deactivate ${test_env}
